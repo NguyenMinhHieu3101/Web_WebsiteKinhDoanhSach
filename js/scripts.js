16ed22bfdd18d00decfc008bd810026d4a198f47
MinhHieu <hieu31012002@gmail.com>
2022-12-02 18:07:04 +0700

    Sắp xong template 1

--- js/scripts.js
+++ js/scripts.js
@@ -73,3 +73,16 @@ $(document).ready(function () {
       ]
     });
   });
+
+  /*ĐĂNG KÝ ĐĂNG NHẬP*/
+  function show_hide_password(target) {
+    var input = document.getElementById("password-input");
+    if (input.getAttribute("type") == "password") {
+        target.classList.add("view");
+        input.setAttribute("type", "text");
+    } else {
+        target.classList.remove("view");
+        input.setAttribute("type", "password");
+    }
+    return false;
+}
