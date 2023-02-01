60f092e6328c038d84046423a9cb0472e90e393a
Đam Sa <20521154@ms.uit.edu.vn>
2023-02-01 22:08:56 +0700

    fix home and init product

---
+++ Template3/js/Home.js
@@ -0,0 +1,12 @@
+document.getElementById('tag1').addEventListener('click', function () {
+    document.body.scrollTop = 1000;
+    document.documentElement.scrollTop = 1000;
+});
+document.getElementById('tag2').addEventListener('click', function () {
+    document.body.scrollTop = 1530;
+    document.documentElement.scrollTop = 1530;
+});
+document.getElementById('tag3').addEventListener('click', function () {
+    document.body.scrollTop = 2100;
+    document.documentElement.scrollTop = 2100;
+});
