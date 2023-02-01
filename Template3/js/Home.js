f87c28980e27a43dea3abe3caab83055d0e3d55b
Đam Sa <20521154@ms.uit.edu.vn>
2023-02-02 00:23:39 +0700

    SaDam responsive home3

--- Template3/js/Home.js
+++ Template3/js/Home.js
@@ -1,12 +1,23 @@
+function getOffset(el) {
+    var _x = 0;
+    var _y = 0;
+    while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+        _x += el.offsetLeft - el.scrollLeft;
+        _y += el.offsetTop - el.scrollTop;
+        el = el.offsetParent;
+    }
+    // console.log(_x);
+
+    return { top: _y, left: _x };
+}
+console.log();
+
 document.getElementById('tag1').addEventListener('click', function () {
-    document.body.scrollTop = 1000;
-    document.documentElement.scrollTop = 1000;
+    document.documentElement.scrollTop = getOffset(document.getElementById('container-1')).top - 50;
 });
 document.getElementById('tag2').addEventListener('click', function () {
-    document.body.scrollTop = 1530;
-    document.documentElement.scrollTop = 1530;
+    document.documentElement.scrollTop = getOffset(document.getElementById('container-2')).top - 50;
 });
 document.getElementById('tag3').addEventListener('click', function () {
-    document.body.scrollTop = 2100;
-    document.documentElement.scrollTop = 2100;
+    document.documentElement.scrollTop = getOffset(document.getElementById('container-3')).top - 50;
 });
